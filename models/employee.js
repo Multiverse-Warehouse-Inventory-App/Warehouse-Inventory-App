e823410f872c1ff85f0887cec1a6a9b25a6d9144
Newman <newmer8@ad.vzwcorp.com>
2021-12-22 00:12:28 -0600

    created signup handlebars and login with bcrypt

--- models/employee.js
+++ models/employee.js
@@ -4,13 +4,20 @@ const { DataTypes, Model } = require("sequelize");
 class Employee extends Model {}
 
 Employee.init(
-    {
-        name: DataTypes.STRING
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    {
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: DataTypes.STRING
+    
+}, {
         sequelize,
         timestamps: false,
     }
 );
 
-module.exports = { Employee }
+module.exports = {Employee}
